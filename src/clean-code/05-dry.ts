fb19deb34fbede2a077a4125aff1fde057c616b4
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2023-08-03 06:43:41 -0500

    Dry implemented

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -7,17 +7,31 @@ class Product {
     public size: Size = '',
   ){};
 
-  toString() {
-    // No DRY
-    if (this.name.length <= 0) throw Error('name is empty');
-    if (this.price <= 0) throw Error('price is zero');
-    if (this.size.length <= 0) throw Error('size is not correctly');
+  isProductReady(): boolean {
+    for (const key in this) {
+      switch(typeof this[key]) {
+        case 'string':
+          if ((<string><unknown>this[key]).length <= 0) throw Error(`${ key } is empty`);
+          break;
+        case 'number':
+          if ((<number><unknown>this[key]) <= 0) throw Error(`${ key } is zero`);
+          break;
+        default:
+          throw Error(`${ typeof this[key] } is not supported`);
+      };
+    };
 
+    return true;
+  };
+
+  toString() {
+    // DRY
+    if (this.isProductReady()) return;
     return `${ this.name } (${ this.price }), ${ this.size }`;
   };
 };
 
 (() => {
-  const bluePants = new Product('Blue large pants', 10, 'M');
+  const bluePants = new Product('Blue large pants');
   console.log(bluePants);
 })();
