7793d2ca6ab5b35a66508b0d2d2ba9f644d5fa5f
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2023-08-04 09:26:08 -0500

    Open close implementation

--- src/solid/02-open-close-a.ts
+++ src/solid/02-open-close-a.ts
@@ -1,14 +1,16 @@
 import { PhotosService, PostService, TodoService } from './02-open-close-b';
+import { HttpClient } from './02-open-close-c';
 
 (async () => {
-  const todoService = new TodoService();
-  const postService = new PostService();
-  const photosService = new PhotosService();
+  const httpClient = new HttpClient();
+
+  const todoService = new TodoService(httpClient);
+  const postService = new PostService(httpClient);
+  const photosService = new PhotosService(httpClient);
 
   const todos = await todoService.getTodoItems();
   const posts = await postService.getPosts();
   const photos = await photosService.getPhotos();
-  
   
   console.log({ todos, posts, photos });
 })();
